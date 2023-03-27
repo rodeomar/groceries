d28009e07e2dfedfa0eea8489c6577a167cbdc9d
rodeomar <raed.alkhanbashi@gmail.com>
2023-03-27 14:30:15 -0700

    add javaScript function to collect userInputs
    Co-authored-by: Emma Gerigscott <elgerig@gmail.com>”

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
-window.addEventListener()
+
+function userInput(e) {
+    e.preventDefault();
+    const inputs = document.querySelectorAll("input[name=groceries]:checked");
+    const inputsArray = Array.from(inputs);
+    inputsArray.forEach(function(item){
+        const paragraph = document.createElement("p");
+        paragraph.append(item.value);
+        document.body.append(paragraph);
+    })
+    console.log(inputsArray)
+}
+window.addEventListener("load", function(){
+document.querySelector("form").addEventListener("submit", userInput)
+});
+
+
+
+
+
+
+
+
+
+
+
+// function handleForm(event) {
+//     event.preventDefault();
+//     const userSelections = document.querySelectorAll("input[name=transportation-option]:checked");
+//     const userSelectionsArray = Array.from(userSelections);
+  
+//     const resultsHeading = document.createElement("h2");
+//     resultsHeading.append("You use the following methods of transportation to travel to work or school:");
+//     document.body.append(resultsHeading);
+  
+//     userSelectionsArray.forEach(function(element) {
+//       const paragraph = document.createElement("p");
+//       paragraph.append(element.value);
+//       document.body.append(paragraph);
+//     });
+//   }
+  
+//   window.addEventListener("load", function() {
+//     document.querySelector("form#transportation_survey").addEventListener("submit", handleForm);
+//   });
